be44f61857b90d0e05ff58ffe0937c93830468ff
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-03-28 03:09:26 -0300

    feat: Reactivity utilization carried out

--- hello-svelte/src/components/Calculadora.svelte
+++ hello-svelte/src/components/Calculadora.svelte
@@ -1,27 +1,73 @@
 <script lang="ts">
   import Botao from "./Botao.svelte";
+  import Linha from "./Linha.svelte";
+  import Tela from "./Tela.svelte";
 
+  class Model {
+    valor: string;
+    constructor(valor: string = "0") {
+      this.valor = valor;
+    }
+
+    inc() {
+      return new Model(`${parseInt(this.valor) + 1}`);
+    }
+  }
+
+  let obj = new Model();
+
+  function numeroDigitado(numero: string) {
+    obj = obj.inc();
+  }
 </script>
 
 <div class="calculadora">
-    <Botao texto="7"/>
-    <Botao texto="8"/>
-    <Botao texto="9"/>
+  <Tela valor={obj.valor} />
+  <Linha>
+    <Botao destaque triplo texto="AC" />
+    <Botao operacao texto="/" />
+  </Linha>
+  <Linha>
+    <Botao texto="7" onClick={numeroDigitado} />
+    <Botao texto="8" onClick={numeroDigitado} />
+    <Botao texto="9" onClick={numeroDigitado} />
+    <Botao operacao texto="*" />
+  </Linha>
+  <Linha>
+    <Botao texto="4" />
+    <Botao texto="5" />
+    <Botao texto="6" />
+    <Botao operacao texto="+" />
+  </Linha>
+  <Linha>
+    <Botao texto="1" />
+    <Botao texto="2" />
+    <Botao texto="3" />
+    <Botao operacao texto="-" />
+  </Linha>
+  <Linha>
+    <Botao duplo texto="0" />
+    <Botao texto="," />
+    <Botao destaque texto="=" />
+  </Linha>
 </div>
 
 <style>
-    :global(body) {
-        background-color: black;
-    }
-    :root {
-        --cor-fundo: #555;
-    }
+  :global(body) {
+    background-color: black;
+  }
 
-    .calculadora {
-        background-color: var(--cor-fundo);
-        height: 220px;
-        width: 150px;
-        display: flex;
-        flex-direction: column;
-    }
+  :root {
+    --cor-fundo: #555;
+  }
+
+  .calculadora {
+    background-color: var(--cor-fundo);
+    height: 220px;
+    width: 150px;
+    padding: 10px;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+  }
 </style>
