a23c4a886105d88762567c5ab3ceeff85b3507b6
Alan Ryan da Silva Domingues <alanryan619@gmail.com>
2025-03-28 01:49:29 -0300

    feat: Created a new project and carried out the scaffolding increment

---
+++ hello-svelte/src/components/Calculadora.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import Botao from "./Botao.svelte";
+
+</script>
+
+<div class="calculadora">
+    <Botao texto="7"/>
+    <Botao texto="8"/>
+    <Botao texto="9"/>
+</div>
+
+<style>
+    :global(body) {
+        background-color: black;
+    }
+    :root {
+        --cor-fundo: #555;
+    }
+
+    .calculadora {
+        background-color: var(--cor-fundo);
+        height: 220px;
+        width: 150px;
+        display: flex;
+        flex-direction: column;
+    }
+</style>
